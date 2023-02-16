740d0644e36315f9106692878479cf6417bbf2b7
gyunt <truth0233@gmail.com>
2023-02-16 13:47:14 +0900

    Add Polygon example.

--- examples/demo_polygon.py
+++ examples/demo_polygon.py
@@ -1,17 +1,34 @@
-#!/usr/bin/env python
+import logging
 
 from cryptofeed import FeedHandler
-from cryptofeed.defines import BYBIT, ORDER_INFO, FILLS
+from cryptofeed.defines import L1_BOOK, CANDLES
+from cryptofeed.exchanges import Polygon
+
+logger = logging.getLogger('feedhandler')
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(stream_handler)
+
+
+async def l1_book(l1, receipt_timestamp):
+    print(f"{receipt_timestamp - l1.timestamp} l1_book update received at {receipt_timestamp}: {l1}")
+
+
+async def candle(candle, receipt_timestamp):
+    print(f"{receipt_timestamp - candle.timestamp} candle update received at {receipt_timestamp}: {candle}")
 
 
 def main():
-    f = FeedHandler(config="config.yaml")
-    f.add_feed(BYBIT,
-               channels=[FILLS, ORDER_INFO],
-               symbols=["ETH-USD-21Z31", "EOS-USD-PERP", "SOL-USDT-PERP"],
-               callbacks={FILLS: fill, ORDER_INFO: order},
-               timeout=-1)
+    path_to_config = 'config.yaml'
+    polygon = Polygon(config=path_to_config,
+                      channels=[CANDLES],
+                      callbacks={CANDLES: candle},
+                      symbols=['USD-KRW'], )
 
+    f = FeedHandler()
+    f.add_feed(polygon)
     f.run()
 
 
