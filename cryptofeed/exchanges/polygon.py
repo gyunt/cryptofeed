2899edb552099659c27d98774d519bc97d93f82a
gyunt <truth0233@gmail.com>
2023-02-16 13:46:46 +0900

    Add candle channel to Polygon.

--- cryptofeed/exchanges/polygon.py
+++ cryptofeed/exchanges/polygon.py
@@ -1,6 +1,5 @@
 import logging
 from decimal import Decimal
-from pprint import pprint
 from typing import Tuple, Dict, List
 
 from yapic import json
@@ -10,14 +9,14 @@ from cryptofeed.connection import RestEndpoint, Routes, WebsocketEndpoint
 from cryptofeed.defines import CANDLES, L1_BOOK, POLYGON
 from cryptofeed.feed import Feed
 from cryptofeed.symbols import Symbol, Symbols
-from cryptofeed.types import L1Book
+from cryptofeed.types import L1Book, Candle
 
 LOG = logging.getLogger('feedhandler')
 
 
 class Polygon(Feed):
     id = POLYGON
-    websocket_endpoints = [WebsocketEndpoint('wss://socket.polygon.io/forex', sandbox=None)]
+    websocket_endpoints = [WebsocketEndpoint('wss://socket.polygon.io/forex')]
     rest_endpoints = [RestEndpoint('https://api.polygon.io', routes=Routes('/v3/reference/tickers?market=fx'))]
     valid_candle_intervals = ('1m',)
 
@@ -80,13 +79,54 @@ class Polygon(Feed):
         )
         await self.callback(L1_BOOK, book, timestamp)
 
+    async def _candle(self, msg: dict, timestamp: float):
+        """
+        {
+          "ev": "CA",               // The event type.
+          "pair": "USD/EUR",        // The currency pair.
+          "o": 0.8687,              // The open price for this aggregate window.
+          "c": 0.86889,             // The close price for this aggregate window.
+          "h": 0.86889,             // The high price for this aggregate window.
+          "l": 0.8686,              // The low price for this aggregate window.
+          "v": 20,                  // The volume of trades during this aggregate window.
+          "s": 1539145740000        // The start time for this aggregate window in Unix Milliseconds.
+                                    // e, The end time for this aggregate window in Unix Milliseconds. (missing)
+        }
+        """
+        c = Candle(self.id,
+                   self.exchange_symbol_to_std_symbol(msg['pair']),
+                   self.timestamp_normalize(msg['s']),
+                   self.timestamp_normalize(msg['s']) + 60,
+                   "1m",
+                   0,
+                   msg['o'],
+                   msg['c'],
+                   msg['h'],
+                   msg['l'],
+                   Decimal(msg['v']),
+                   None,
+                   self.timestamp_normalize(msg['s']),
+                   raw=msg)
+        await self.callback(CANDLES, c, timestamp)
+
     async def message_handler(self, msg: str, conn, timestamp: float):
         messages = json.loads(msg, parse_float=Decimal)
 
         for msg in messages:
             if 'ev' in msg:
-                if msg['ev'] == 'C':
+                if msg['ev'] == 'status':
+                    if msg['status'] == 'connected':
+                        LOG.info("%s: Connected to Polygon", conn.uuid)
+                    elif msg['status'] == 'auth_success':
+                        LOG.info("%s: Authentication successful", conn.uuid)
+                    elif msg['status'] == 'success':
+                        LOG.info("%s: Subscription to %s successful", conn.uuid, msg['message'])
+                    else:
+                        LOG.warning("%s: Subscription to %s failed: %s", conn.uuid, msg['message'], msg)
+                elif msg['ev'] == 'C':
                     await self._quote(msg, timestamp)
+                elif msg['ev'] == 'CA':
+                    await self._candle(msg, timestamp)
                 else:
                     LOG.warning("%s: Unknown message in msg_dict: %s", conn.uuid, msg)
             else:
